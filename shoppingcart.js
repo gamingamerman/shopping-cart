2500015986b4bc72bbe6f1efa133904f9f351f85
gamingamerman <56639436+gamingamerman@users.noreply.github.com>
2021-06-30 15:50:15 +0100

    Add files via upload

--- shoppingcart.js
+++ shoppingcart.js
@@ -2,7 +2,7 @@ let cart = [
     {
         name : "banana",
         qty : 3,
-        price : 3.99
+        price : 4.99
     },
     {
         name : "chicken",
@@ -12,49 +12,70 @@ let cart = [
     {
         name : "apple",
         qty : 5,
-        price : 3.99
+        price : 2.99
     },
     {
         name : "milk",
         qty : 1,
-        price : 3.99
+        price : 1.70
     },
     {
         name : "pork",
         qty : 7,
-        price : 3.99
+        price : 3.79
     },
     {
         name : "orange",
         qty : 2,
-        price : 3.99
+        price : 3.00
     },
     {
         name : "toothpaste",
         qty : 1,
-        price : 3.99
+        price : 1.50
     },
 ]
 
-function mulitply ( obj ) {
-    total = obj[0].price * obj[0].qty
-    return total
 
-}
-
-function total( obj ) {
+function sum_res ( obj ) {
     sum = 0;
+    sum_1 = []
 
-    for( let prices in obj) {
-        sum += parseFloat( obj[0].price );
+    for (let items in obj) {
+        result = obj[items].price * obj[items].qty
+        sum += result
     }
+    return sum
 
-    return sum;
 }
 
-// console.log(total(cart))
-console.log(mulitply(cart))
+function mult_res( obj ) {
+    sum_1 = []
 
+    for (let items in obj) {
+        result = obj[items].price * obj[items].qty
+        sum_1.push(result)
+    }
+    return sum_1
+}
+
+// function give_name( obj ) {
+//     stuff = 
+// }
+
+result1 = sum_res(cart)
+result2 = mult_res(cart)
+
+function drawer( res1, res2, cart ) {
+    for (let item in cart) {
+        console.log("Item " + item + ": " + cart[item].name + " " + cart[item].price + "€ " + cart[item].qty)
+    
+    }
+    console.log("---------------------")
+    console.log("Total: " + cart.length + " " + "Items: ")
+}
+
+console.log(drawer(result1, result2, cart))
 // banana : 3.99,
 // apple : 2.99,
 // milk : 2.50,
